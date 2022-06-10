523bd17176e5c371c846b663e9cd81ce5425df85
Connor Stevens <connor.stevens@ingka.ikea.com>
2022-06-10 13:57:48 +0200

    github actions step

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,23 @@
+# pylint: disable=missing-docstring
+# [...]
 from flask import Flask
 from flask_restx import Api
-
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
-
+    from config import Config
+    app.config.from_object(Config)
+    db.init_app(app)
+    
+    from app.models import Tweet
+    from flask_migrate import Migrate
+    
+    migrate = Migrate(app, db)
+    
     from .apis.tweets import api as tweets
     api = Api()
     api.add_namespace(tweets)
     api.init_app(app)
-
     app.config['ERROR_404_HELP'] = False
     return app
--- app/apis/tweets.py
+++ app/apis/tweets.py
@@ -1,11 +1,25 @@
 # pylint: disable=missing-docstring
+from flask_restx import Namespace, Resource, fields, abort
+from app.models import Tweet
+from app import db
 
-from flask_restx import Namespace, Resource
+api = Namespace('tweets')
 
-api = Namespace("tweets")
+json_tweet = api.model('Tweet', {
+    'id': fields.Integer,
+    'text': fields.String,
+    'created_at': fields.DateTime
+})
 
 
-@api.route("/hello")
+@api.route('/<int:id>')
+@api.response(404, 'Tweet not found')
+@api.param('id', 'The tweet unique identifier')
 class TweetResource(Resource):
-    def get(self):
-        return "Hello from the 'tweets' namespace!"
+    @api.marshal_with(json_tweet)
+    def get(self, id):
+        tweet = db.session.query(Tweet).get(id)
+        if tweet is None:
+            api.abort(404)
+        else:
+            return tweet
